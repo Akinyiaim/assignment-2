ca92d0da1b1130e72c6562a8c1888d10d51d1f71
Rachael-Maina <124576181+Rachael-Maina@users.noreply.github.com>
2023-02-15 17:38:01 +0300

    Question1

---
+++ number1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   char start;
+   start='a';
+   while(start <='z'){
+        putchar(start);
+        start++;
+   }
+    putchar('\n');
+    return 0;
+}
